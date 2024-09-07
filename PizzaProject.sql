04d75126affee4c9e67b6aa231478c3e202df603
jainsajal0708 <128510317+JainSajal0708@users.noreply.github.com>
2024-09-07 23:15:10 +0530

    Add files via upload

--- PizzaProject.sql
+++ PizzaProject.sql
@@ -1,6 +1,12 @@
 use pizza_project;
+create table Orders(Order_id int Primary key, Order_date date not null,Time time not null);
+drop table Orders;
+desc Orders;
 
+create table Order_Details(Order_Details_id int Primary key, Order_id int not null,Pizza_Id Varchar(50) not null,Quantity int not null);
+desc Order_details;
 
+select * from orders;
 select * from pizzas;
 
 
